662be6fbf86f1b638bf9370b92954fdd48fd3029
Januario95 <januariocipriano@gmail.com>
2022-06-19 01:27:37 +0200

    Remove unnecessay packages

--- apiproject/urls.py
+++ apiproject/urls.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView
+# )
 
 
 urlpatterns = [
@@ -16,8 +16,8 @@ urlpatterns = [
 
     # path('api/v1/', include('apiapp.urls')),
     # path('', include('apiapp.portal_urls', namespace='apiapp')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth-login/', include('rest_framework.urls')),
 
     # path('api/v1/app/', include('apiapp.mobile_app_urls')),
